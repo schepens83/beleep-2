e043aa9d4dacac827ea65830442ed74cdf4eafa1
schepens83 <schepens@gmail.com>
2017-10-09 17:00:39 +0200

    fix webpacker js folder and dependencies

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,8 +1,5 @@
 import Vue from 'vue/dist/vue.esm'
 import moment from 'moment'
-import Calculator from './calculator.vue'
-import AppCanvas from './canvas-main.vue'
-import ToDo from './todo.vue'
 import DayScore from './day-score/day-score.vue'
 
 
@@ -11,9 +8,6 @@ document.addEventListener('DOMContentLoaded', () => {
     el: '#main',
     data: {},
     components: {
-      Calculator,
-      "app-canvas": AppCanvas,
-      'to-do': ToDo,
       'day-score': DayScore
     }
   })
