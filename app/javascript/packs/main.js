89d599a46df42e69554c1ab1c1f11e2c30c31469
schepens83 <schepens@gmail.com>
2017-10-13 11:46:04 +0200

    add moment again

--- app/javascript/packs/main.js
+++ app/javascript/packs/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue/dist/vue.esm'
 import DayScore from './day-score/day-score.vue'
 
-// import moment from 'moment';
-// Vue.prototype.$moment = moment
+import moment from 'moment';
+Vue.prototype.$moment = moment
 
 
 document.addEventListener('DOMContentLoaded', () => {
